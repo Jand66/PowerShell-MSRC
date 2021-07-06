5a9708bff64b9f68496262c184eda76aadabb7cc
Jand66 <56920668+Jand66@users.noreply.github.com>
2021-07-06 13:56:02 +0200

    Create msrcbasics.ps1

---
+++ msrcbasics.ps1
@@ -0,0 +1,16 @@
+  
+### Install the module from the PowerShell Gallery (must be run as Admin)
+Install-Module -Name MsrcSecurityUpdates -force
+Import-Module MsrcSecurityUpdates
+
+### Download the March CVRF as an object
+$cvrfDoc = Get-MsrcCvrfDocument -ID 2021-Jun
+
+### Get the Remediations of Type 'Workaround' (0)
+$cvrfDoc.Vulnerability.Remediations | Where Type -EQ 0
+
+### Get the Remediations of Type 'Mitigation' (1)
+$cvrfDoc.Vulnerability.Remediations | Where Type -EQ 1
+
+### Get the Remediations of Type 'VendorFix' (2)
+$cvrfDoc.Vulnerability.Remediations | Where Type -EQ 2 
