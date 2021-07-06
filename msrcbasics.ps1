bc01a275feee732e966be25ed9444d0eb8c6b4fc
Jand66 <56920668+Jand66@users.noreply.github.com>
2021-07-06 15:11:30 +0200

    Update msrcbasics.ps1

--- msrcbasics.ps1
+++ msrcbasics.ps1
@@ -16,3 +16,10 @@ $cvrfDoc.Vulnerability.Remediations | Where Type -EQ 1
 $cvrfDoc.Vulnerability.Remediations | Where Type -EQ 2 
 
 $cvrfDoc | Get-MsrcVulnerabilityReportHtml | out-file -FilePath c:\jun2021.html
+$secupdates = Get-MsrcSecurityUpdate
+$cvrfdoc = Get-MsrcCvrfDocument -id "2021-Jun"
+$affectedsoft = Get-MsrcCvrfAffectedSoftware -Vulnerability $cvrfDoc.Vulnerability -ProductTree $cvrfDoc.ProductTree
+$cvesummary = Get-MsrcCvrfCVESummary -Vulnerability $cvrfdoc.Vulnerability -ProductTree $cvrfdoc.ProductTree
+$exploitability = Get-MsrcCvrfExploitabilityIndex -Vulnerability $cvrfdoc.Vulnerability
+$secbulletinhtml = Get-MsrcSecurityBulletinHtml -Vulnerability $cvrfdoc.Vulnerability -ProductTree $cvrfdoc.ProductTree -DocumentTracking $cvrfdoc.DocumentTracking -DocumentTitle $cvrfdoc.DocumentTitle
+$vulnrep = Get-MsrcVulnerabilityReportHtml -Vulnerability $cvrfdoc.Vulnerability -ProductTree $Cvrfdoc.ProductTree
