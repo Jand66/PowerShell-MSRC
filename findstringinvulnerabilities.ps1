a5a3c536291850cb839efc42f520d3606292d535
Jand66 <56920668+Jand66@users.noreply.github.com>
2021-07-06 14:42:00 +0200

    Update findstringinvulnerabilities.ps1

--- findstringinvulnerabilities.ps1
+++ findstringinvulnerabilities.ps1
@@ -1,41 +1,56 @@
-﻿### Install the module from the PowerShell Gallery (must be run as Admin)
-# Install-Module -Name MsrcSecurityUpdates -force
-
-### Download the March CVRF as an object
-# $cvrfDoc = Get-MsrcCvrfDocument -ID 2021-Jun
-
-### Get the Remediations of Type 'Workaround' (0)
-#$cvrfDoc.Vulnerability.Remediations | Where Type -EQ 0
-
-### Get the Remediations of Type 'Mitigation' (1)
-#$cvrfDoc.Vulnerability.Remediations | Where Type -EQ 1
-
-### Get the Remediations of Type 'VendorFix' (2)
-#$cvrfDoc.Vulnerability.Remediations | Where Type -EQ 2 
-
-# $cvrfDoc | Get-MsrcVulnerabilityReportHtml | out-file -FilePath c:\jun2021.html
-
-# Select-String -pattern "bootloader" -path C:\jun2021.html
-
 $months = @("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov", "Dec")
 $years =@("2017","2018","2019","2020","2021")
-$searchstring = "boot"
-foreach($year in $years)
+# testvalues limited to save time
+# $months = @("Jan","Feb","Mar","Apr","May","Jun")
+# $years =@("2021")
+$searchstring = "tpm"
+$rootpath = "c:\CVEDocs\"
+
+Function createfiles
+#create inputfiles - this could take some time
 {
-    foreach($month in $months)
+    Param(
+        [parameter(Mandatory=$true)][Array] $years, 
+        [parameter(Mandatory=$true)][Array] $months, 
+        [parameter(Mandatory=$true)][String] $rootpath)
+    foreach($year in $years)
     {
-        $docID = $year+"-"+$month
-        $infile = "c:\"+ $year + $month + ".html"
-        $outfile = "c:\"+ $year + $month + "_" + $searchstring + ".txt"
-        #$cvrfdoc = Get-MsrcCvrfDocument -ID $docID
-        #$cvrfDoc | Get-MsrcVulnerabilityReportHtml | out-file -FilePath $filename
-        $infile
-        $result = Select-String -pattern $searchstring -path $infile 
-        if ($result -ne $null)
-        { 
-         $result | out-file -FilePath $outfile
-         $outfile
+        foreach($month in $months)
+        {
+            $docID = $year+"-"+$month
+            $infile = $rootpath + $year + $month + ".html"
+            $cvrfdoc = Get-MsrcCvrfDocument -ID $docID
+            $cvrfDoc | Get-MsrcVulnerabilityReportHtml | out-file -FilePath $infile
+            $infile
         }
     }
 }
 
+Function analysefiles
+#create outputfiles that contain the lines where searchstring was found
+{
+    Param(
+        [parameter(Mandatory=$true)][Array] $years, 
+        [parameter(Mandatory=$true)][Array] $months, 
+        [parameter(Mandatory=$true)][String] $rootpath, 
+        [parameter(Mandatory=$true)][String] $searchstring
+        )
+    foreach($year in $years)
+    {
+        foreach($month in $months)
+        {
+            $docID = $year+"-"+$month
+            $infile = $rootpath + $year + $month + ".html"
+            $outfile = $rootpath + $year + $month + "_" + $searchstring + ".txt"
+            $infile
+            $result = Select-String -pattern $searchstring -path $infile 
+            if ($result -ne $null)
+            { 
+             $result | out-file -FilePath $outfile
+             $outfile
+            }
+        }
+    }
+}
+createfiles -years $years -months $months -rootpath $rootpath
+analysefiles -years $years -months $months -rootpath $rootpath -searchstring $searchstring 
